d8ed8c67d851a3381a961a2fd8c9952369b07adb
Desi y Lucas <desimorenoh@gmail.com>
2022-02-03 15:06:04 +0100

    reajuste de name en sample data y test

--- src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
+++ src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
@@ -41,15 +41,15 @@ class Bftp2EconotravelServerApplicationTests {
         mockMvc.perform(get("/api/experiences"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[*]", hasSize(2)))
-                .andExpect(jsonPath("$[0].name", equalTo("Experiencia 1: Paseo en bicicleta por el Montseny")))
-                .andExpect(jsonPath("$[1].name", equalTo("Experiencia 2: Descubre la costa en barco de vela")))
+                .andExpect(jsonPath("$[0].name", equalTo("Paseo en bicicleta por el Montseny")))
+                .andExpect(jsonPath("$[1].name", equalTo("Descubre la costa en barco de vela")))
                 .andDo(print());
     }
 
     private void addSampleExperiences() {
         List<Experience> experiences = List.of(
-                new Experience("Experiencia 1: Paseo en bicicleta por el Montseny", "Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny. Una escapada veraniega perfecta para parejas, familias y amigos que nos permitirá conocer nuevos y sorprendentes lugares. El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando asfalto y pistas anchas, culminando en un mirador con magníficas vistas del Vallés y el mar. Desde aquí continuaremos la bajada combinando senderos, pistas y algún tramo de carretera y terreno mixto para visitar la zona de Santa Fe, donde descubriremos la Ermita y el Bosque de las Secuoyas. Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos de una excelente comida casera con butifarra blanca y negra y munxetes del Montseny.", 250, "5h", "Actividad disponible para todas las edades. Disponemos de bicicletas accesibles para personas con movilidad reducida en el tren inferior así como sillines con capacidad para niños menores de 5 años (peso máximo 20kg).", "Montaña, bicicleta, excursión larga."),
-                new Experience("Experiencia 2: Descubre la costa en barco de vela", "Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona. Una escapada veraniega apta incluso para los días más calurosos del año. Descubre los encantadores alrededores de la ciudad de Barcelona y visita desde el mar sus más impresionantes playas y calas. Comenzaremos la excursión en el Puerto de Barcelona, desde donde partiremos hacia el norte para visitar playas como la Mar Bella, la Playa de la Mora y la Playa de los Pescadores. A bordo de la embarcación podremos disfrutar de una selección de quesos y embutidos catalanes acompañada de cava brut y zumos de frutas. Asimismo, pararemos cerca de la Playa de Montgat para realizar una actividad de buceo de superficie que nos permitirá apreciar la diversidad de la fauna marítima local y su ecosistema. Finalizaremos la excursión en el mismo puerto del que partimos.", 280, "4h", "Actividad disponible para todas las edades. Pasarela para silla de ruedas disponible bajo reserva.", "Playa, barco, excursión larga.")
+                new Experience("Paseo en bicicleta por el Montseny", "Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny. Una escapada veraniega perfecta para parejas, familias y amigos que nos permitirá conocer nuevos y sorprendentes lugares. El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando asfalto y pistas anchas, culminando en un mirador con magníficas vistas del Vallés y el mar. Desde aquí continuaremos la bajada combinando senderos, pistas y algún tramo de carretera y terreno mixto para visitar la zona de Santa Fe, donde descubriremos la Ermita y el Bosque de las Secuoyas. Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos de una excelente comida casera con butifarra blanca y negra y munxetes del Montseny.", 250, "5h", "Actividad disponible para todas las edades. Disponemos de bicicletas accesibles para personas con movilidad reducida en el tren inferior así como sillines con capacidad para niños menores de 5 años (peso máximo 20kg).", "Montaña, bicicleta, excursión larga."),
+                new Experience("Descubre la costa en barco de vela", "Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona. Una escapada veraniega apta incluso para los días más calurosos del año. Descubre los encantadores alrededores de la ciudad de Barcelona y visita desde el mar sus más impresionantes playas y calas. Comenzaremos la excursión en el Puerto de Barcelona, desde donde partiremos hacia el norte para visitar playas como la Mar Bella, la Playa de la Mora y la Playa de los Pescadores. A bordo de la embarcación podremos disfrutar de una selección de quesos y embutidos catalanes acompañada de cava brut y zumos de frutas. Asimismo, pararemos cerca de la Playa de Montgat para realizar una actividad de buceo de superficie que nos permitirá apreciar la diversidad de la fauna marítima local y su ecosistema. Finalizaremos la excursión en el mismo puerto del que partimos.", 280, "4h", "Actividad disponible para todas las edades. Pasarela para silla de ruedas disponible bajo reserva.", "Playa, barco, excursión larga.")
         );
 
         experienceRepository.saveAll(experiences);
@@ -60,20 +60,20 @@ class Bftp2EconotravelServerApplicationTests {
 
         mockMvc.perform(post("/api/experiences/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\": \"Experiencia 1: Paseo en bicicleta por el Montseny\"}")
+                .content("{\"name\": \"Paseo en bicicleta por el Montseny\"}")
         ).andExpect(status().is(200));
 
         var experiences = experienceRepository.findAll();
 
         assertThat(experiences, contains(
-                hasProperty("name", is("Experiencia 1: Paseo en bicicleta por el Montseny"))
+                hasProperty("name", is("Paseo en bicicleta por el Montseny"))
         ));
     }
 
     @Test
 
     void allowsToDeleteAnExperience() throws Exception {
-        Experience experience = experienceRepository.save(new Experience("Experiencia 2: Descubre la costa en barco de vela", "Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona. Una escapada veraniega apta incluso para los días más calurosos del año. Descubre los encantadores alrededores de la ciudad de Barcelona y visita desde el mar sus más impresionantes playas y calas. Comenzaremos la excursión en el Puerto de Barcelona, desde donde partiremos hacia el norte para visitar playas como la Mar Bella, la Playa de la Mora y la Playa de los Pescadores. A bordo de la embarcación podremos disfrutar de una selección de quesos y embutidos catalanes acompañada de cava brut y zumos de frutas. Asimismo, pararemos cerca de la Playa de Montgat para realizar una actividad de buceo de superficie que nos permitirá apreciar la diversidad de la fauna marítima local y su ecosistema. Finalizaremos la excursión en el mismo puerto del que partimos.", 280, "4h", "Actividad disponible para todas las edades. Pasarela para silla de ruedas disponible bajo reserva.", "Playa, barco, excursión larga."));
+        Experience experience = experienceRepository.save(new Experience("Descubre la costa en barco de vela", "Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona. Una escapada veraniega apta incluso para los días más calurosos del año. Descubre los encantadores alrededores de la ciudad de Barcelona y visita desde el mar sus más impresionantes playas y calas. Comenzaremos la excursión en el Puerto de Barcelona, desde donde partiremos hacia el norte para visitar playas como la Mar Bella, la Playa de la Mora y la Playa de los Pescadores. A bordo de la embarcación podremos disfrutar de una selección de quesos y embutidos catalanes acompañada de cava brut y zumos de frutas. Asimismo, pararemos cerca de la Playa de Montgat para realizar una actividad de buceo de superficie que nos permitirá apreciar la diversidad de la fauna marítima local y su ecosistema. Finalizaremos la excursión en el mismo puerto del que partimos.", 280, "4h", "Actividad disponible para todas las edades. Pasarela para silla de ruedas disponible bajo reserva.", "Playa, barco, excursión larga."));
         mockMvc.perform(delete("/api/experiences/delete/" + experience.getId()))
                 .andExpect(status().is(200));
 
